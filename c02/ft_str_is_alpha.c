b87112d25237d2afdec61b0492eb68691e434f70
VIRTUORR <THE-VIRTUORR@users.noreply.github.com>
2021-11-14 22:23:21 +0100

    c02 commit

--- c02/ft_str_is_alpha.c
+++ c02/ft_str_is_alpha.c
@@ -1,3 +1,6 @@
+//	Created by aazdoev  //
+//----------------------//
+
 #include <stdio.h>
 
 int	ft_str_is_alpha(char *str)
@@ -6,11 +9,14 @@ int	ft_str_is_alpha(char *str)
 	// A-Z == 65-90	
 	// a-z == 97-122
 
-	if (str[0] == '\0') {
+	if (!str[0]) {
 		return (1);
-	} else {
+	}
+	else
+	{
 		i = 0;
-		while (str[i]) {
+		while (str[i])
+		{
 			if (str[i] < 65 || (str[i] > 90 && str[i] < 97) || str[i] > 122 )
 				return (0);
 			i++;
