75dcfcb2c55202727950da6e9fb13e94b6df4cad
VIRTUORR <THE-VIRTUORR@users.noreply.github.com>
2021-11-15 09:53:36 +0100

    c02 commit

---
+++ c02/ft_strlowcase.c
@@ -0,0 +1,27 @@
+//	Created by aazdoev  //
+//----------------------//
+//
+//0-9 == 48-57
+
+#include <stdio.h>
+
+char	*ft_strupcase(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 65 && str[i] <= 90)
+			str[i] += 32;
+		i++;
+	}
+	return (str);
+}
+
+int main(void)
+{
+	char str[] = "sGjfd45*&$%(@_)2KGDIdf";
+	printf("%s", ft_strupcase(str));
+	//printf("%c", 'j' - 32);
+}
